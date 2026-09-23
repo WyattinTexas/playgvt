c677334a8d287f54e2e9ff6177ba263fbcbd2ccc
WyattinTexas <153330268+WyattinTexas@users.noreply.github.com>
2026-09-23 17:24:32 -0400

    The Ice Caves' hideout, round two: the boss yeti now comes and goes from the dark of his tunnel (his eyes glow where he stood), the savages hold their swords and spears in hand, the girl wears her pink bow on her head, and the watchers' eyes come out of black holes. Client v11.81.

--- bench/stamp.js
+++ bench/stamp.js
@@ -1,1 +1,1 @@
-window.__BENCH_STAMP={"cv":"11.80","md5":"6c76f7017039f033e3d5c3c3cff86587","sha":"8769e4bbd","at":"2026-09-23T20:36:53Z","src":"playgvt-staged","pre":"8158be3dc4","js":"8a8d7d0c0e","bench":3};
+window.__BENCH_STAMP={"cv":"11.81","md5":"c72b07d3f68cf15052b8348dbcc7b290","sha":"f1779f7a4","at":"2026-09-23T21:24:33Z","src":"playgvt-staged","pre":"8158be3dc4","js":"8a8d7d0c0e","bench":3};
--- navigator/stamp.js
+++ navigator/stamp.js
@@ -1,1 +1,1 @@
-window.__NAV_STAMP={"cv":"11.80","md5":"6c76f7017039f033e3d5c3c3cff86587","sha":"8769e4bbd","at":"2026-09-23T20:36:54Z","src":"playgvt-staged","pre":"b65068b8cc","js":"a09ba2fc1c","nav":1};
+window.__NAV_STAMP={"cv":"11.81","md5":"c72b07d3f68cf15052b8348dbcc7b290","sha":"f1779f7a4","at":"2026-09-23T21:24:34Z","src":"playgvt-staged","pre":"b65068b8cc","js":"a09ba2fc1c","nav":1};
