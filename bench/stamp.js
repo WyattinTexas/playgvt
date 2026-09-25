be12facfdf1acaeb1ef415fe5987390b007214b1
WyattinTexas <153330268+WyattinTexas@users.noreply.github.com>
2026-09-25 18:56:23 -0400

    Princess Croak's harp is fuller: one real harp, every shot a plucked string, her crit a quick rising roll, the face shot a rolled arch up and back down.

--- bench/stamp.js
+++ bench/stamp.js
@@ -1,1 +1,1 @@
-window.__BENCH_STAMP={"cv":"12.43","md5":"a628f17c27c7e9443ba115c9cdb662d1","sha":"0c84f6302","at":"2026-09-25T22:20:13Z","src":"playgvt-staged","pre":"8158be3dc4","js":"8a8d7d0c0e","bench":3};
+window.__BENCH_STAMP={"cv":"12.44","md5":"ed04d1bf2110c232f6e64cab3df97a11","sha":"1953986ab","at":"2026-09-25T22:56:23Z","src":"playgvt-staged","pre":"8158be3dc4","js":"8a8d7d0c0e","bench":3};
--- navigator/stamp.js
+++ navigator/stamp.js
@@ -1,1 +1,1 @@
-window.__NAV_STAMP={"cv":"12.43","md5":"a628f17c27c7e9443ba115c9cdb662d1","sha":"0c84f6302","at":"2026-09-25T22:20:14Z","src":"playgvt-staged","pre":"b65068b8cc","js":"a09ba2fc1c","nav":1};
+window.__NAV_STAMP={"cv":"12.44","md5":"ed04d1bf2110c232f6e64cab3df97a11","sha":"1953986ab","at":"2026-09-25T22:56:24Z","src":"playgvt-staged","pre":"b65068b8cc","js":"a09ba2fc1c","nav":1};
